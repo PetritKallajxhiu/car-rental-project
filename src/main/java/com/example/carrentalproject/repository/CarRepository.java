8767c4960015f2f7dc369af547d625e42c1c0fe3
Petrit Kallajxhiu <petrit.kallajxhiu@fti.edu.al>
2021-05-18 22:51:08 +0200

    Comment post, ect commit

--- src/main/java/com/example/carrentalproject/repository/CarRepository.java
+++ src/main/java/com/example/carrentalproject/repository/CarRepository.java
@@ -3,6 +3,7 @@ package com.example.carrentalproject.repository;
 import com.example.carrentalproject.model.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -12,8 +13,6 @@ public interface CarRepository extends JpaRepository<Car, Integer> {
     @Query(value = "SELECT * FROM cars WHERE featured = true", nativeQuery = true)
     List<Car> findAllFeatured();
 
-
-public interface CarRepository extends JpaRepository<Car, Integer> {
     @Query(value = "SELECT c FROM Car c WHERE c.name = :name")
     List<Car> search(@Param("name") String name);
 
